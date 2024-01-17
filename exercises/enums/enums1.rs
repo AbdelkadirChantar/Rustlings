551a142d8d038c15ff2d7fbea368f0b9afd044b5
abdelkadir <abdelkadir.chantar@tuni.fi>
2024-01-17 14:45:56 +0200

    ALL DONE!

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -1,17 +1,19 @@
 // enums1.rs
-//
 // No hints this time! ;)
 
-// I AM NOT DONE
+
 
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit,
+    Echo,
+    ChangeColor,
 }
 
 fn main() {
     println!("{:?}", Message::Quit);
     println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
+    println!("{:?}", Message::Echo);
     println!("{:?}", Message::ChangeColor);
 }
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,23 +1,26 @@
 // strings3.rs
-//
-// Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
-// hint.
+// Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+   let mut result = String::from(input.trim());
+   result
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut res = String::from(input);
+    res.push_str(" world!");
+    res
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let res = input.replace("cars","balloons");
+    res
+    
 }
 
 #[cfg(test)]
